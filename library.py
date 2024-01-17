590c965bab0985ad88cca72cc0caf77477a35e2e
Kevin Maurizi <kevin.maurizi@bzz.ch>
2024-01-17 09:43:18 +0100

    changed to read_int in main

--- library.py
+++ library.py
@@ -25,8 +25,8 @@ def show_balance(books):
     print('Print the book balance here')
 
 
-def read_int(prompt):
-    pass
+def read_int(prompt, minimum, maximum):
+    return int(input(prompt)) #Placeholder to let main run. Delete when doing the aassignment
 
 
 def read_date(prompt):
@@ -40,17 +40,14 @@ def main():
         print("1. Add Rental")
         print("2. Show Balances")
         print("3. Exit")
-        choice = input("Enter your choice (1/2/3): ").strip()
+        choice = read_int('Enter your choice (1/2/3): ',1,3)
 
-        if choice == '1':
+        if choice == 1:
             add_rental(books)
-        elif choice == '2':
+        elif choice == 2:
             show_balance(books)
-        elif choice == '3':
+        elif choice == 3:
             break
-        else:
-            print("Invalid choice. Please choose 1, 2, or 3.")
-
 
 if __name__ == "__main__":
     main()
